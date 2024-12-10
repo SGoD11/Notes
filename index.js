100db536b965fb6f4203424577018298f4538e5c
subhajit dhar <subhajit13dhar@gmail.com>
2024-12-10 11:59:26 +0530

    some changes

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
+app.get("/delete",(req,res)=>{
+    res.send("Delete");
+
+});
+app.get("/edit",(req,res)=>{
+    res.send("Edit");
+    console.log(req);
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
