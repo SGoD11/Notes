1d2faed3ef1fff3f6f437799f0d2db74ac321a9c
subhajit dhar <subhajit13dhar@gmail.com>
2024-12-13 12:54:24 +0530

    Successfull in connection of db

--- server.js
+++ server.js
@@ -1,38 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 4000;
 
-// In-memory data store
-let posts = [
-  {
-    id: 1,
-    heading: "The Rise of Decentralized Finance",
-    title:
-      "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
-    
-    date: "2023-08-01T10:00:00Z",
-  },
-  {
-    id: 2,
-    heading: "The Impact of Artificial Intelligence on Modern Businesses",
-    title:
-      "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
-   
-    date: "2023-08-05T14:30:00Z",
-  },
-  {
-    id: 3,
-    heading: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
-    title:
-      "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
-   
-    date: "2023-08-10T09:15:00Z",
-  },
-];
+//database
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "NotePad",
+  password: "sdhar",
+  port: 5432,
+});
+db.connect();
 
-let lastId = 3;
+async function fetchData() {
+  const result = await db.query('SELECT * FROM public."NoteePad"');
+  console.log("This is result", result.rows);
+  return result.rows;
+}
+
+// In-memory data store
+let posts = await fetchData();
+
+let lastId = posts.length;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
